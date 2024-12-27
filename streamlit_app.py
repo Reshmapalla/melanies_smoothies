dc8a0b76e8a014c7ebe19a07b187eb6d407e2eef
Reshmapalla <reshmapalla2@gmail.com>
2024-12-27 15:06:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ', '.join(ingredients_list)  # Create a comma-separated string of ingredients
-    for fruit_chosen in ingredients_list
+    for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
